1039915003c039cc555ba5b06a13a952f53e7b96
floh-bhaptics <floh@fahrenbergers.de>
2022-01-09 13:48:43 +0100

    Added Teleport and pouch effects

--- FloorPlan2_bhaptics.cs
+++ FloorPlan2_bhaptics.cs
@@ -8,6 +8,9 @@ using MelonLoader;
 using HarmonyLib;
 using MyBhapticsTactsuit;
 
+using UnityEngine;
+using TButt;
+
 namespace FloorPlan2_bhaptics
 {
     public class FloorPlan2_bhaptics : MelonMod
@@ -32,30 +35,21 @@ namespace FloorPlan2_bhaptics
             }
         }
 
-        [HarmonyPatch(typeof(PantsWaist), "HandGrabbed", new Type[] { typeof(FloorPlanHand) })]
+        [HarmonyPatch(typeof(PantsWaist), "SetGrabbingHand", new Type[] { typeof(FloorPlanHand) })]
         public class bhaptics_GrabPouch
         {
             [HarmonyPostfix]
-            public static void Postfix()
+            public static void Postfix(PantsWaist __instance)
             {
-                tactsuitVr.LOG("Open pouch");
-                tactsuitVr.StartPouch();
+                //tactsuitVr.LOG("Open pouch");
+                if (__instance.IsGrabbed) tactsuitVr.StartPouch();
+                else tactsuitVr.StopPouch();
             }
         }
 
-        [HarmonyPatch(typeof(PantsWaist), "HandReleased", new Type[] { typeof(FloorPlanHand) })]
-        public class bhaptics_ReleasePouch
-        {
-            [HarmonyPostfix]
-            public static void Postfix()
-            {
-                tactsuitVr.LOG("Close pouch");
-                tactsuitVr.StopPouch();
-            }
-        }
 
         
-        [HarmonyPatch(typeof(ButtHandSkinData), "PlayFart", new Type[] { typeof(TButt.TBInput.Controller) })]
+        [HarmonyPatch(typeof(ButtHandSkinData), "PlayFart", new Type[] { typeof(TBInput.Controller) })]
         public class bhaptics_PlayFart
         {
             [HarmonyPostfix]
@@ -67,7 +61,7 @@ namespace FloorPlan2_bhaptics
             }
         }
 
-        [HarmonyPatch(typeof(HandSkinData), "PlayGrabSound", new Type[] { typeof(UnityEngine.Transform), typeof(HandSkinData.HandID) })]
+        [HarmonyPatch(typeof(HandSkinData), "PlayGrabSound", new Type[] { typeof(Transform), typeof(HandSkinData.HandID) })]
         public class bhaptics_HandGrab
         {
             [HarmonyPostfix]
@@ -78,7 +72,7 @@ namespace FloorPlan2_bhaptics
             }
         }
         
-        [HarmonyPatch(typeof(PlayerHandSelfActions), "Clap", new Type[] { typeof(UnityEngine.Vector3) })]
+        [HarmonyPatch(typeof(PlayerHandSelfActions), "Clap", new Type[] { typeof(Vector3) })]
         public class bhaptics_HandClap
         {
             [HarmonyPostfix]
@@ -90,44 +84,54 @@ namespace FloorPlan2_bhaptics
                 tactsuitVr.PlaybackHaptics("ClapArms");
             }
         }
-        
-        [HarmonyPatch(typeof(PlayerHandSelfActions), "ThumbsUp", new Type[] { typeof(TButt.TBInput.Controller) })]
+
+        [HarmonyPatch(typeof(PlayerHandSelfActions), "ThumbsUp", new Type[] { typeof(TBInput.Controller) })]
         public class bhaptics_ThumbsUp
         {
             [HarmonyPostfix]
-            public static void Postfix(TButt.TBInput.Controller controller)
+            public static void Postfix(TBInput.Controller controller)
             {
                 tactsuitVr.LOG("ThumbsyUppy");
                 bool isRightHand = false;
-                if (controller == TButt.TBInput.Controller.RHandController) isRightHand = true;
+                if (controller == TBInput.Controller.RHandController) isRightHand = true;
                 tactsuitVr.HandEffect("ThumbsUp", isRightHand);
             }
         }
 
-        [HarmonyPatch(typeof(PlayerHandSelfActions), "ThumbsDown", new Type[] { typeof(TButt.TBInput.Controller) })]
+        [HarmonyPatch(typeof(PlayerHandSelfActions), "ThumbsDown", new Type[] { typeof(TBInput.Controller) })]
         public class bhaptics_ThumbsDown
         {
             [HarmonyPostfix]
-            public static void Postfix(TButt.TBInput.Controller controller)
+            public static void Postfix(TBInput.Controller controller)
             {
                 bool isRightHand = false;
-                if (controller == TButt.TBInput.Controller.RHandController) isRightHand = true;
+                if (controller == TBInput.Controller.RHandController) isRightHand = true;
                 tactsuitVr.HandEffect("ThumbsDown", isRightHand);
             }
         }
 
-        [HarmonyPatch(typeof(PlayerHandSelfActions), "Smack", new Type[] { typeof(TButt.TBInput.Controller), typeof(UnityEngine.Vector3) })]
+        [HarmonyPatch(typeof(PlayerHandSelfActions), "Smack", new Type[] { typeof(TBInput.Controller), typeof(Vector3) })]
         public class bhaptics_Smack
         {
             [HarmonyPostfix]
-            public static void Postfix(TButt.TBInput.Controller controller)
+            public static void Postfix(TBInput.Controller controller)
             {
                 bool isRightHand = false;
-                if (controller == TButt.TBInput.Controller.RHandController) isRightHand = true;
+                if (controller == TBInput.Controller.RHandController) isRightHand = true;
                 tactsuitVr.HandEffect("Smack", isRightHand);
             }
         }
-
+        
+        [HarmonyPatch(typeof(TButt.Locomotion.TBTeleportManager), "TeleportToPoint", new Type[] { typeof(TButt.Locomotion.TBTeleportPoint), typeof(bool) })]
+        public class bhaptics_Teleport
+        {
+            [HarmonyPostfix]
+            public static void Postfix(bool instantly)
+            {
+                if (!instantly) tactsuitVr.PlaybackHaptics("TeleportThrough");
+            }
+        }
+        
 
     }
 }
